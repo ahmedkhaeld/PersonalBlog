fc953b75a461b2a36e8297b7af6f9c3f93938cb3
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-05-17 16:39:58 +0200

    Feat Add Categories From the db
    Fetch the categories from the database

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,33 +20,25 @@
 
 <!-- Blog Categories Well -->
 <div class="well">
-    <h4>Blog Categories</h4>
-    <div class="row">
-        <div class="col-lg-6">
-            <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.col-lg-6 -->
-        <div class="col-lg-6">
-            <ul class="list-unstyled">
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-                <li><a href="#">Category Name</a>
-                </li>
-            </ul>
-        </div>
+            <?php 
+                 $query = "SELECT * FROM categories ";
+                 $select_categories_sidebar = mysqli_query($connection, $query);
+            ?>
+                    <h4>Blog Categories</h4>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <ul class="list-unstyled">
+                            <?php 
+                             while ($row = mysqli_fetch_assoc($select_categories_sidebar)){
+                                $cat_title = $row['cat_title'];
+                                $cat_id = $row['cat_id'];
+
+                                echo " <li><a href='category.php?category=$cat_id'> {$cat_title}</a></li>";
+                             }
+                            ?>
+                               
+                            </ul>
+                        </div>
         <!-- /.col-lg-6 -->
     </div>
     <!-- /.row -->
