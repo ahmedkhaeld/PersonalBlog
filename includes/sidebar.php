8df2fca6f4617317140dbe0df9e8ec022905f7ed
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-05-19 16:15:49 +0200

    Feat Add feature to sidebar to dispaly posts
    user can click category to view all posts for that category by putting
     key that dynamically contains posts inside the link then diplay
    
     Note: looks at sidebar.txt inside Docs

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,36 +18,36 @@
                 </div>
 
 
-<!-- Blog Categories Well -->
-<div class="well">
-            <?php 
-                 $query = "SELECT * FROM categories ";
-                 $select_categories_sidebar = mysqli_query($connection, $query);
-            ?>
-                    <h4>Blog Categories</h4>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <ul class="list-unstyled">
-                            <?php 
-                             while ($row = mysqli_fetch_assoc($select_categories_sidebar)){
-                                $cat_title = $row['cat_title'];
-                                $cat_id = $row['cat_id'];
-
-                                echo " <li><a href='category.php?category=$cat_id'> {$cat_title}</a></li>";
-                             }
-                            ?>
-                               
-                            </ul>
-                        </div>
-        <!-- /.col-lg-6 -->
+    <!-- Blog Categories Well -->
+    <div class="well"> 
+                <?php 
+                    $query = "SELECT * FROM categories ";
+                    $select_categories_sidebar = mysqli_query($connection, $query);
+                ?>
+                        <h4>Blog Categories</h4>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <ul class="list-unstyled">
+                                <?php 
+                                while ($row = mysqli_fetch_assoc($select_categories_sidebar)){
+                                    $cat_title = $row['cat_title'];
+                                    $cat_id = $row['cat_id'];
+                                    // send user to another page that display all posts equal to this category
+                                    echo " <li><a href='category.php?category=$cat_id'> {$cat_title}</a></li>";
+                                }
+                                ?>
+                                
+                                </ul>
+                            </div>
+            <!-- /.col-lg-6 -->
+        </div>
+        <!-- /.row -->
     </div>
-    <!-- /.row -->
-</div>
 
-<!-- Side Widget Well -->
-<div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-</div>
+    <!-- Side Widget Well -->
+    <div class="well">
+        <h4>Side Widget Well</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+    </div>
 
 </div>
