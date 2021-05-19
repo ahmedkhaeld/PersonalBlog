d790457d4a48999b500da45dcb44c79c0ecfbd36
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-05-19 15:54:52 +0200

    Feat Add some navigatin links in index page
    post title and post image direct user to its page
    Home button redirect to index page

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ while ($row = mysqli_fetch_assoc($select_all_posts_query)){
 
                 <!-- First Blog Post -->
                 <h2>
-                <a href="#"><?php echo $post_title;  ?></a>
+                <a href="post.php?p_id=<?php echo $post_id;  ?>"><?php echo $post_title;  ?></a>
                 </h2>
                 <p class="lead">
                 by <a href="#"><?php echo $post_author;  ?></a>
@@ -44,8 +44,9 @@ while ($row = mysqli_fetch_assoc($select_all_posts_query)){
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date;?></p>
                 <hr>
                 <!-- look at Docs/postimage.txt -->
+                <a href="post.php?p_id=<?php echo $post_id;  ?>">
                 <img class="img-responsive" src="images/<?php echo $post_image ;?>" alt="">
-                <hr>
+                </a>
                 <p> <?php echo $post_content ; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
